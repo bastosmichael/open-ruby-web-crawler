4931396927d3286b04291436f46ec4319d360514
Michael Bastos <bastosmichael@gmail.com>
2013-12-01 08:05:30 -0800

    removed parse

--- lib/schemaorg.rb
+++ lib/schemaorg.rb
@@ -8,23 +8,24 @@
 module Crawl
   class SchemaOrg #< Parse
         def initialize page
-            @page = page
-            @id = Digest::MD5.hexdigest(@page.url.to_s)
-            @url = @page.doc.css("link[@rel='canonical']").first['href'] if !@url rescue nil
-            @url = @page.url.to_s if !@url
-            @image = @page.doc.css("link[@rel='image_src']").first['href'] if !@image rescue nil
-            @name = @page.doc.at('title').inner_html rescue nil
-            self.build
+      @page = page
+      @id = Digest::MD5.hexdigest(@page.url.to_s) if !@id
+      @url = @page.doc.css("link[@rel='canonical']").first['href'] if !@url rescue nil
+        @url = @page.url.to_s if !@url
+        @name = @page.doc.at('title').inner_html if !@name rescue nil
+        @description = @page.doc.css("meta[@name='description']").first['content'] if !@description rescue nil
+        @mobile_url = @page.doc.css("link[@media='handheld']").first['href'] if !@mobile_url rescue nil
+      self.build
+    end
+    
+      def save
+        remove_instance_variable(:@page)
+        hash = {}
+        instance_variables.each do |var| 
+          hash[var.to_s.delete("@")] = instance_variable_get(var) 
         end
-        
-        def save
-          remove_instance_variable(:@page)
-          hash = {}
-          instance_variables.each do |var| 
-            hash[var.to_s.delete("@")] = instance_variable_get(var) 
-          end
-          hash
-        end
+        hash
+    end
         
     def build
       # schema = @page.doc.css('//*[contains(@itemtype, "schema.org")]').first["itemtype"]
--- lib/scrape.rb
+++ lib/scrape.rb
@@ -2,7 +2,27 @@
 require 'digest/md5'
 
 module Crawl
-  class Scrape < Parse
+  class Scrape #< Parse
+    def initialize page
+      @page = page
+      @id = Digest::MD5.hexdigest(@page.url.to_s) if !@id
+      @url = @page.doc.css("link[@rel='canonical']").first['href'] if !@url rescue nil
+        @url = @page.url.to_s if !@url
+        @name = @page.doc.at('title').inner_html if !@name rescue nil
+        @description = @page.doc.css("meta[@name='description']").first['content'] if !@description rescue nil
+        @mobile_url = @page.doc.css("link[@media='handheld']").first['href'] if !@mobile_url rescue nil
+      self.build
+    end
+    
+      def save
+        remove_instance_variable(:@page)
+        hash = {}
+        instance_variables.each do |var| 
+          hash[var.to_s.delete("@")] = instance_variable_get(var) 
+        end
+        hash
+    end
+    
     def build
       self.methods.grep(/scrape/).each do |s|
         self.send(s) 
